0a2f555b4b28f6bb09edf4b55ac37dacd5f6c46d
paolo.archiv <paolo.archiv@gmail.com>
2025-09-16 16:03:38 +0000

    Challenge 2  - PHP Function

---
+++ php-intro/func.php
@@ -0,0 +1,53 @@
+<?php
+
+function replaceVowelsWithX($str)
+{
+    $vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+    foreach ($vowels as $vowel) {
+        $str = str_replace($vowel, 'x', $str);
+    }
+    return $str;
+}
+
+if (isset($_GET['word'])) {
+    $word = $_GET['word'];
+} else {
+    $word = '';
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <title>Vowel Replacer</title>
+</head>
+
+<body>
+    <main class="container py-5">
+        <h1>Vowel Replacer</h1>
+        <form class="mt-3">
+            <div class="form-group">
+                <label for="word">Input a word</label>
+                <input type="text" class="form-control" id="word" name="word" placeholder="Your word" value="<?= $word; ?>">
+            </div>
+            <button type="submit" class="btn btn-primary mt-3">Submit</button>
+        </form>
+        <?php
+        if (isset($_GET['word'])) {
+            // echo var_dump($_GET);
+            $word = $_GET['word'];
+            $changedWord = replaceVowelsWithX($word);
+            echo "<p class='mt-3'>Result: " . $changedWord . "</p>";
+        }
+        ?>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+</body>
+
+</html>
