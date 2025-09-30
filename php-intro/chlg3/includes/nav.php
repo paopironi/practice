194a268de47bc1d87d83453f4ba7d2e01923f5d0
paolo.archiv <paolo.archiv@gmail.com>
2025-09-30 14:11:54 +0000

    Add link to home page to logo

--- php-intro/chlg3/includes/nav.php
+++ php-intro/chlg3/includes/nav.php
@@ -16,7 +16,7 @@
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid text-uppercase" style="padding-left: 1.5rem;">
-            <a href="../index.php" class="navbar-brand">MK TIME</a>
+            <a href="/" class="navbar-brand">MK TIME</a>
 
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
@@ -26,7 +26,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" href="../index.php">Products</a>
+                        <a class="nav-link" href="/">Products</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="../create.php">Create</a>
